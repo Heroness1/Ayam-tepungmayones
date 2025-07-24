90ce008e8909a3b5f35d18351540dc8a4eda2700
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-07-25 00:48:09 +0700

    Update index.js

--- Apology-to-diah/src/index.js
+++ Apology-to-diah/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css"; // 
 import ApologyToDiah from "./ApologyToDiah";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
