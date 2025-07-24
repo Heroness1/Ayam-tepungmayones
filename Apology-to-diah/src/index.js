8f818925fb7f17adb56d0e466c2ac36a4fe419c6
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-07-25 00:34:52 +0700

    Create index.js

---
+++ Apology-to-diah/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ApologyToDiah from "./ApologyToDiah";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ApologyToDiah />
+  </React.StrictMode>
+);
